077f5bfd95ab37e44006b2d0954fec17b6d76c69
Susanna Anil <55010363+sanil72900@users.noreply.github.com>
2022-11-11 19:22:01 +0000

    need to fix make test

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import logic.fantasy as fantasy
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Draft 2022."}
+
 
 @app.get("/draft/{team_name}")
 async def draft_team(team_name):
@@ -16,10 +18,12 @@ async def draft_team(team_name):
     my_team.create_roster()
     return my_team.roster
 
+
 @app.get("/sub/{position_to_sub}")
 async def sub_players(position_to_sub):
     my_team.sub_player(position_to_sub)
     return my_team.roster
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, host="0.0.0.0")
